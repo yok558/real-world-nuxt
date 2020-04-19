285c73636add75f8b64f34cc9353fcd916c76f43
yok558 <yok.box@gmail.com>
2020-04-19 14:13:46 +0300

    Static Site Generated Deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import EventService from './services/EventService'
+
 export default {
   mode: 'universal',
   /*
@@ -56,5 +58,14 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  generate: {
+    routes: () => {
+      return EventService.getEvents().then((response) => {
+        return response.data.map((event) => {
+          return '/event/' + event.id
+        })
+      })
+    }
   }
 }
